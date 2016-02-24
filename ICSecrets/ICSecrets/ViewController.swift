7ed7b9a6d1234233060e9f5ae707ab3025ca33a8
atttali <atttali@me.com>
2016-02-24 16:30:00 +0000

    Initial commit

---
+++ ICSecrets/ICSecrets/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ICSecrets
+//
+//  Created by Ali Attar on 24/02/2016.
+//  Copyright © 2016 Ali Attar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    print("Hello")
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
